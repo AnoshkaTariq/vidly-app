b3d1872c9a0fad476d69c5d7995e472c03006e67
AnoshkaTariq <anoshkatariq193@gmail.com>
2021-02-02 14:31:41 +0500

    updated connection string

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const winston = require('winston');
-const config = require('config');
 
 module.exports = function () {
-    const db = config.get('db');
-    mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    mongoose.connect('mongodb://localhost:27017/vidly', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
         .then(() => winston.info('Connected to mongoDB'));
 };
